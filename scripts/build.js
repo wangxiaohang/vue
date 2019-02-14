2f8a74ba4bdd51912cb93518d93892108c9ac8ac
yuxiao <xiaohang.wang@aliyun.com>
2019-02-14 16:43:43 +0800

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -8,10 +8,11 @@ if (!fs.existsSync('dist')) {
   fs.mkdirSync('dist')
 }
 
+/* 获取config.js中的配置项 */
 let builds = require('./config').getAllBuilds()
 
-// filter builds via command line arg
-if (process.argv[2]) {
+/* 对配置项builds做过滤 start */
+if (process.argv[2]) { // process Node.js程序中的全局变量
   const filters = process.argv[2].split(',')
   builds = builds.filter(b => {
     return filters.some(f => b.output.file.indexOf(f) > -1 || b._name.indexOf(f) > -1)
@@ -22,7 +23,9 @@ if (process.argv[2]) {
     return b.output.file.indexOf('weex') === -1
   })
 }
+/* 对配置项builds做过滤 end */
 
+/* 基于配置项的构建过程 */
 build(builds)
 
 function build (builds) {
